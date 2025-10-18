41769e32e5b4a5ff6460b7ee767b8a63a264ea02
Karol_Czernecki <inz.karol.czernecki@gmail.com>
2025-10-18 16:09:47 +0200

    Dodaj pliki projektów.

---
+++ MementoMori/Form1.Designer.cs
@@ -0,0 +1,146 @@
+﻿namespace MementoMori
+{
+    partial class Form1
+    {
+        /// <summary>
+        ///  Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        ///  Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        ///  Required method for Designer support - do not modify
+        ///  the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            dtp_DayOfBirth = new DateTimePicker();
+            dtp_CurrentDate = new DateTimePicker();
+            label1 = new Label();
+            label2 = new Label();
+            button1 = new Button();
+            comboBox_Country = new ComboBox();
+            SuspendLayout();
+            // 
+            // dtp_DayOfBirth
+            // 
+            dtp_DayOfBirth.Anchor = AnchorStyles.Top | AnchorStyles.Bottom | AnchorStyles.Left | AnchorStyles.Right;
+            dtp_DayOfBirth.CustomFormat = "dd-MMM-yyyy";
+            dtp_DayOfBirth.Format = DateTimePickerFormat.Custom;
+            dtp_DayOfBirth.ImeMode = ImeMode.NoControl;
+            dtp_DayOfBirth.Location = new Point(50, 400);
+            dtp_DayOfBirth.MaximumSize = new Size(120, 23);
+            dtp_DayOfBirth.MinimumSize = new Size(120, 23);
+            dtp_DayOfBirth.Name = "dtp_DayOfBirth";
+            dtp_DayOfBirth.Size = new Size(120, 23);
+            dtp_DayOfBirth.TabIndex = 0;
+            // 
+            // dtp_CurrentDate
+            // 
+            dtp_CurrentDate.Anchor = AnchorStyles.Top | AnchorStyles.Bottom | AnchorStyles.Left | AnchorStyles.Right;
+            dtp_CurrentDate.CustomFormat = "dd-MMM-yyyy";
+            dtp_CurrentDate.Format = DateTimePickerFormat.Custom;
+            dtp_CurrentDate.ImeMode = ImeMode.NoControl;
+            dtp_CurrentDate.Location = new Point(621, 400);
+            dtp_CurrentDate.MaximumSize = new Size(120, 23);
+            dtp_CurrentDate.MinimumSize = new Size(120, 23);
+            dtp_CurrentDate.Name = "dtp_CurrentDate";
+            dtp_CurrentDate.Size = new Size(120, 23);
+            dtp_CurrentDate.TabIndex = 1;
+            // 
+            // label1
+            // 
+            label1.Anchor = AnchorStyles.Top | AnchorStyles.Bottom | AnchorStyles.Left | AnchorStyles.Right;
+            label1.AutoSize = true;
+            label1.Location = new Point(50, 382);
+            label1.MaximumSize = new Size(69, 15);
+            label1.MinimumSize = new Size(69, 15);
+            label1.Name = "label1";
+            label1.Size = new Size(69, 15);
+            label1.TabIndex = 3;
+            label1.Text = "Day of birth";
+            // 
+            // label2
+            // 
+            label2.Anchor = AnchorStyles.Top | AnchorStyles.Bottom | AnchorStyles.Left | AnchorStyles.Right;
+            label2.AutoSize = true;
+            label2.Location = new Point(621, 382);
+            label2.MaximumSize = new Size(73, 15);
+            label2.MinimumSize = new Size(73, 15);
+            label2.Name = "label2";
+            label2.Size = new Size(73, 15);
+            label2.TabIndex = 4;
+            label2.Text = "Current date";
+            // 
+            // button1
+            // 
+            button1.Anchor = AnchorStyles.Top | AnchorStyles.Bottom | AnchorStyles.Left | AnchorStyles.Right;
+            button1.Location = new Point(361, 400);
+            button1.MaximumSize = new Size(75, 23);
+            button1.MinimumSize = new Size(75, 23);
+            button1.Name = "button1";
+            button1.Size = new Size(75, 23);
+            button1.TabIndex = 5;
+            button1.Text = "Calculate";
+            button1.UseVisualStyleBackColor = true;
+            button1.Click += button1_Click;
+            // 
+            // comboBox_Country
+            // 
+            comboBox_Country.Anchor = AnchorStyles.Top | AnchorStyles.Bottom | AnchorStyles.Left | AnchorStyles.Right;
+            comboBox_Country.FormattingEnabled = true;
+            comboBox_Country.Items.AddRange(new object[] { "Albania", "Austria", "Belarus", "Belgium", "Bosnia and Herzegovina", "Bulgaria", "Croatia", "Cyprus", "Czechia", "Denmark", "Estonia", "Finland", "France", "Germany", "Greece", "Hungary", "Iceland", "Ireland", "Italy", "Kosovo", "Latvia", "Liechtenstein", "Lithuania", "Luxembourg", "Malta", "Moldova", "Montenegro", "Netherlands", "North Macedonia", "Norway", "Poland", "Portugal", "Romania", "Russia", "Serbia", "Slovakia", "Slovenia", "Spain", "Sweden", "Switzerland", "Ukraine", "United Kingdom" });
+            comboBox_Country.Location = new Point(50, 35);
+            comboBox_Country.MaximumSize = new Size(121, 0);
+            comboBox_Country.MinimumSize = new Size(121, 0);
+            comboBox_Country.Name = "comboBox_Country";
+            comboBox_Country.Size = new Size(121, 23);
+            comboBox_Country.TabIndex = 6;
+            comboBox_Country.Text = "Country";
+            // 
+            // Form1
+            // 
+            AutoScaleDimensions = new SizeF(7F, 15F);
+            AutoScaleMode = AutoScaleMode.Font;
+            BackgroundImage = (Image)resources.GetObject("$this.BackgroundImage");
+            BackgroundImageLayout = ImageLayout.Stretch;
+            ClientSize = new Size(757, 445);
+            Controls.Add(comboBox_Country);
+            Controls.Add(button1);
+            Controls.Add(label2);
+            Controls.Add(label1);
+            Controls.Add(dtp_CurrentDate);
+            Controls.Add(dtp_DayOfBirth);
+            MaximumSize = new Size(773, 484);
+            MinimumSize = new Size(773, 484);
+            Name = "Form1";
+            Text = "Memento mori v0.1.0";
+            ResumeLayout(false);
+            PerformLayout();
+        }
+
+        #endregion
+
+        private DateTimePicker dtp_DayOfBirth;
+        private DateTimePicker dtp_CurrentDate;
+        private Label label1;
+        private Label label2;
+        private Button button1;
+        private ComboBox comboBox_Country;
+    }
+}
---
+++ MementoMori/Form1.cs
@@ -0,0 +1,105 @@
+namespace MementoMori
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private Dictionary<string, double> LifetimeExpentancy = new Dictionary<string, double>
+        {
+                {   "Albania" ,   79.1    }   ,
+                {   "Austria" ,   81.5    }   ,
+                {   "Belarus" ,   74.4    }   ,
+                {   "Belgium" ,   82.5    }   ,
+                {   "Bosnia and Herzegovina"  ,   77.9    }   ,
+                {   "Bulgaria"    ,   75.8    }   ,
+                {   "Croatia" ,   78.6    }   ,
+                {   "Cyprus"  ,   82.5    }   ,
+                {   "Czechia" ,   80.0    }   ,
+                {   "Denmark" ,   81.9    }   ,
+                {   "Estonia" ,   78.8    }   ,
+                {   "Finland" ,   81.7    }   ,
+                {   "France"  ,   83.1    }   ,
+                {   "Germany" ,   81.2    }   ,
+                {   "Greece"  ,   81.6    }   ,
+                {   "Hungary" ,   76.9    }   ,
+                {   "Iceland" ,   82.6    }   ,
+                {   "Ireland" ,   82.6    }   ,
+                {   "Italy"   ,   83.8    }   ,
+                {   "Kosovo"  ,   78.0    }   ,
+                {   "Latvia"  ,   75.9    }   ,
+                {   "Liechtenstein"   ,   84.8    }   ,
+                {   "Lithuania"   ,   77.3    }   ,
+                {   "Luxembourg"  ,   83.4    }   ,
+                {   "Malta"   ,   83.6    }   ,
+                {   "Moldova" ,   71.2    }   ,
+                {   "Montenegro"  ,   73.8    }   ,
+                {   "Netherlands" ,   82.0    }   ,
+                {   "North Macedonia" ,   73.2    }   ,
+                {   "Norway"  ,   83.1    }   ,
+                {   "Poland"  ,   78.6    }   ,
+                {   "Portugal"    ,   82.4    }   ,
+                {   "Romania" ,   76.6    }   ,
+                {   "Russia"  ,   73.2    }   ,
+                {   "Serbia"  ,   75.2    }   ,
+                {   "Slovakia"    ,   78.1    }   ,
+                {   "Slovenia"    ,   82.0    }   ,
+                {   "Spain"   ,   84.0    }   ,
+                {   "Sweden"  ,   83.4    }   ,
+                {   "Switzerland" ,   84.2    }   ,
+                {   "Ukraine" ,   73.4    }   ,
+                {   "United Kingdom"  ,   81.3    }
+            };
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            if (comboBox_Country.SelectedItem != null)
+            {
+                string selectedItem = comboBox_Country.SelectedItem.ToString();
+                double lifetimeExpentacyForChoosenCountry = LifetimeExpentancy[selectedItem];
+                int calculated_age = CalculateAge();
+                double lifetimeLeft = lifetimeExpentacyForChoosenCountry - calculated_age;
+
+                if (calculated_age > 0)
+                {
+                    if (lifetimeLeft > 0)
+                    {
+                        MessageBox.Show("Statistic lifetime expentacy for " + selectedItem + " is approx. " +
+                                        lifetimeExpentacyForChoosenCountry + " years.\nYou are " + $"{calculated_age:F0}" + " years old.\nSo you have technicaly " + lifetimeLeft +
+                                        " years left.\n Use it wisely :) ");
+                    }
+                    else
+                    {
+                        MessageBox.Show("Statistic lifetime expentacy for " + selectedItem + " is approx. " +
+                                       lifetimeExpentacyForChoosenCountry + " years.\nYou are " + $"{calculated_age:F0}" + " years old.\nSo technicaly You should be dead :) ");
+                    }
+                }
+            }
+            else
+            {
+                MessageBox.Show("Country check ERROR!");
+            }
+        }
+
+        private int CalculateAge()
+        {
+            int age = 0;
+            if (dtp_CurrentDate.Value > dtp_DayOfBirth.Value)
+            {
+                age = dtp_CurrentDate.Value.Year - dtp_DayOfBirth.Value.Year;
+                if ( (age + dtp_DayOfBirth.Value.Year) < dtp_CurrentDate.Value.Year)
+                {
+                    age--;
+                }
+            }
+            else
+            {
+                MessageBox.Show("Current date can't be erlier than day of birth!");
+                return -1;
+            }
+            return age;
+        }
+    }
+}
